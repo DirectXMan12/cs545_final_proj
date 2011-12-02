fa89d64d2dcc8369b9c20cee0698b66712ccd9a5
Solly Ross <solly@memolane.com>
2011-12-01 19:55:23 -0500

    added in err_macros.h file

---
+++ err_macros.h
@@ -0,0 +1,3 @@
+#define POST_WARN(str) std::cout << "\e[33m" << "Warning: " << str << "\e[0m" << std::endl
+#define POST_ERR(str) std::cerr << "\e[1;31m" << "Error: " << str << "\e[0m" << std::endl
+#define POST_INFO(str) std::cout << "\e[32m" << str << "\e[0m" << std::endl
